c736fdd7f6c77544aee528d592905dfcb30f597f
Lucas-nmp <lucas.morandeira.parejo@gmail.com>
2024-07-22 20:16:23 +0200

    Añadir la parte Front del proyecto en React

--- src/main/java/lm/Pizzeria/controller/Controller.java
+++ src/main/java/lm/Pizzeria/controller/Controller.java
@@ -1,6 +1,7 @@
 package lm.Pizzeria.controller;
 
 import java.util.List;
+import lm.Pizzeria.model.Customer;
 import lm.Pizzeria.model.Product;
 import lm.Pizzeria.service.CustomerService;
 import lm.Pizzeria.service.IngredientService;
@@ -11,8 +12,10 @@ import lm.Pizzeria.service.ProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -48,9 +51,16 @@ public class Controller {
     
     @GetMapping("/products")
     public List<Product> viewProducts() {
+        // buscar productos por categoria en lugar de todos
         var products = productService.getAllProducts();
         products.forEach((product -> logger.info(product.toString())));
         return products;
+    }
+    
+    @GetMapping("/customer/{phone}")
+    public ResponseEntity<Customer> getCustomerByPhone(@PathVariable String phone) {
+        Customer customer = customerService.findCustomerByPhone(phone);
+        return ResponseEntity.ok(customer);
     }
     
     
